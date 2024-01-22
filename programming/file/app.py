7f80ca9eabe365bf53426ab312b784bfed574e4d
shch989 <tankpop11@gmail.com>
2024-01-23 00:10:25 +0900

    시범 테스트 완료

--- programming/file/app.py
+++ programming/file/app.py
@@ -1,17 +1,12 @@
-# 프롬프트 엔지니어링에 관한 실용을 정리해둔 문서입니다.
-
-## 기본 템플릿
-```
 import os
 import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 completion = openai.ChatCompletion.create(
-  model = "gpt-3.5-turbo",
+  model="gpt-3.5-turbo",
   messages=[
     {"role": "user", "content": "Hello!"}
   ]
 )
 
 print(completion.choices[0].message)
-```
